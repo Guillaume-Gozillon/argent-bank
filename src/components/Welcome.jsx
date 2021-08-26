6bbe5ce30f8675be7fc0e0bb46efdfc841262298
Guillaume <guillaumegozillon@gmail.com>
2021-08-27 00:45:40 +0200

    Import Backend +  init CRA

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,19 @@
+import { useHistory } from "react-router-dom"
+import useFetch from "../useFetch"
+
+const Welcome = () => {
+
+    const BASE_URL = `http://localhost:3001/api/v1/user/signup`
+
+    const user = useFetch(BASE_URL)
+
+    console.log(user);
+
+    return (
+        <div>
+            <h2>It works!!</h2>
+        </div>
+    )
+}
+
+export default Welcome
