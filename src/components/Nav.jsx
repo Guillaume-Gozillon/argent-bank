a5cee431fe4a590ba89a2e564779a61e4387598d
Guillaume <guillaumegozillon@gmail.com>
2021-09-20 12:29:13 +0200

    CSS

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,9 @@
 import logo from '../img/logo.png'
 import { Link } from 'react-router-dom'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
 const Nav = () => {
   // REMOVE FONTAWESOM
   // FINIR LA VERSION MOBILE
@@ -10,8 +13,8 @@ const Nav = () => {
       <Link to='/'>
         <img src={logo} alt='logo' />
       </Link>
-      <div className='login'>
-        <p className='logimg'>🧑‍💻</p>
+      <div className='loginNav'>
+        <FontAwesomeIcon className='signImg' icon={faUserCircle} />
         <Link to='/login'>Sign In</Link>
       </div>
     </nav>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 import axios from 'axios'
 import { useState } from 'react'
@@ -38,30 +40,39 @@ const Login = () => {
   if (isAuth) return <Redirect to='/profil' />
 
   return (
-    <div>
+    <div className='login'>
       <Nav />
-      <form id='sign-up-form'>
-        <label htmlFor='email'>Email</label>
-        <br />
-        <input
-          type='text'
-          name='email'
-          id='email'
-          onChange={handleEmail}
-          value={email}
-        />
-        <br />
-        <label htmlFor='password'>Mot de passe</label>
-        <br />
-        <input
-          type='password'
-          name='password'
-          id='password'
-          onChange={handlePassword}
-          value={password}
-        />
-        <button onClick={submitLogin}>Se connecter</button>
-      </form>
+      <main className='form'>
+        <div className='form-container'>
+          <FontAwesomeIcon className='logimg' icon={faUserCircle} />
+          <h1>Sign In</h1>
+          <form id='sign-up-form'>
+            <label htmlFor='email'>Username</label>
+            <br />
+            <input
+              type='text'
+              name='email'
+              id='email'
+              onChange={handleEmail}
+              value={email}
+            />
+            <br />
+            <label htmlFor='password'>Password</label>
+            <br />
+            <input
+              type='password'
+              name='password'
+              id='password'
+              onChange={handlePassword}
+              value={password}
+            />
+            <br />
+            <input type="checkbox" name="Remember me" id="" />
+            <br />
+            <button onClick={submitLogin}>Sign In</button>
+          </form>
+        </div>
+      </main>
       <Footer />
     </div>
   )
